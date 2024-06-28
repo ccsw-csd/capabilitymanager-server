a3fc5181a3a1c0771c571cce5ba751b5706a27db
ignacife <ignacife@capgemini.com>
2024-06-28 10:15:00 +0200

    Fix mantenimiento Itinerarios

--- src/main/java/com/ccsw/capabilitymanager/mantenimientoitinerariosformativos/MantenimientoItinerariosFormativosServiceImpl.java
+++ src/main/java/com/ccsw/capabilitymanager/mantenimientoitinerariosformativos/MantenimientoItinerariosFormativosServiceImpl.java
@@ -19,88 +19,81 @@ import jakarta.transaction.Transactional;
 
 @Service
 @Transactional
-public class MantenimientoItinerariosFormativosServiceImpl implements MantenimientoItinerariosFormativosService{
+public class MantenimientoItinerariosFormativosServiceImpl implements MantenimientoItinerariosFormativosService {
 
 	@Autowired
 	private MantenimientoItinerariosFormativosRepository mantenimientoItinerariosFormativosRepository;
-	
+
 	@Override
 	public void save(ItinerariosFormativosDto dto) throws ParseException {
 
-		
 		comprobarExistenciaCodigo(dto.getCodigo());
-		
-		ItinerariosFormativos itinerariosFormativo = new ItinerariosFormativos();
-		
-        Date fechaActual = new Date();
-        String fechaMaxBaja = "9999-12-31";
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        formatter.format(fechaActual);
-        Date fechaEspecifica = formatter.parse(fechaMaxBaja);
-        
-        itinerariosFormativo.setCodigo(dto.getCodigo());
-        itinerariosFormativo.setName(dto.getName());
-        itinerariosFormativo.setUsuario(dto.getUsuario());
-        itinerariosFormativo.setFecha_Alta(fechaActual);
-        itinerariosFormativo.setFecha_Baja(fechaEspecifica);
 
-		
+		ItinerariosFormativos itinerariosFormativo = new ItinerariosFormativos();
+
+		Date fechaActual = new Date();
+		String fechaMaxBaja = "9999-12-31";
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		formatter.format(fechaActual);
+		Date fechaEspecifica = formatter.parse(fechaMaxBaja);
+
+		itinerariosFormativo.setCodigo(dto.getCodigo());
+		itinerariosFormativo.setName(dto.getName());
+		itinerariosFormativo.setUsuario(dto.getUsuario());
+		itinerariosFormativo.setFecha_Alta(fechaActual);
+		itinerariosFormativo.setFecha_Baja(fechaEspecifica);
+
 		mantenimientoItinerariosFormativosRepository.save(itinerariosFormativo);
 	}
-	
+
 	@Override
 	public void update(ItinerariosFormativosDto dto) throws ParseException {
+		// Find the existing entity by codigo
+		ItinerariosFormativos existingItinerario = mantenimientoItinerariosFormativosRepository.findByid(dto.getId());
 		
+		if(!dto.getCodigo().equals(existingItinerario.getCodigo())) {
 		comprobarExistenciaCodigo(dto.getCodigo());
+		}
+		// Update the fields
+		existingItinerario.setName(dto.getName());
+		existingItinerario.setUsuario(dto.getUsuario());
+		existingItinerario.setCodigo(dto.getCodigo());
+		Date fechaActual = new Date();
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		formatter.format(fechaActual);
+		existingItinerario.setFecha_Modif(fechaActual);
+		existingItinerario.setUsuario(dto.getUsuario());
 
-	    // Find the existing entity by codigo
-	    ItinerariosFormativos existingItinerario = mantenimientoItinerariosFormativosRepository.findByid(dto.getId());
-	    if (existingItinerario != null) {
-	        // Update the fields
-	        existingItinerario.setName(dto.getName());
-	        existingItinerario.setUsuario(dto.getUsuario());
-	        existingItinerario.setCodigo(dto.getCodigo());
-	        Date fechaActual = new Date();
-	        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-	        formatter.format(fechaActual);
-	        existingItinerario.setFecha_Modif(fechaActual);
-	        existingItinerario.setUsuario(dto.getUsuario());
+		// Save the updated entity
+		mantenimientoItinerariosFormativosRepository.save(existingItinerario);
 
-	        // Save the updated entity
-	        mantenimientoItinerariosFormativosRepository.save(existingItinerario);
-	    } else {
-	        // Handle case where no entity with given codigo is found
-	        throw new RuntimeException("No se encontró ningún itinerario con el código: " + dto.getCodigo());
-	    }
 	}
 
 	@Override
 	public List<ItinerariosFormativos> findAll() {
-		return (List<ItinerariosFormativos>) this.mantenimientoItinerariosFormativosRepository.findAll().stream().sorted().toList();
+		return (List<ItinerariosFormativos>) this.mantenimientoItinerariosFormativosRepository.findAll().stream()
+				.sorted().toList();
 	}
 
 	@Override
 	public void delete(Long id) {
-		
+
 		ItinerariosFormativos itinerario = mantenimientoItinerariosFormativosRepository.findByid(id);
-		
+
 		mantenimientoItinerariosFormativosRepository.delete(itinerario);
-		
+
 	}
-	
+
 	@Override
 	public void comprobarExistenciaCodigo(String codigo) {
-		
-	    ItinerariosFormativos existingItinerario = mantenimientoItinerariosFormativosRepository.findByCodigo(codigo);
 
-	    if (existingItinerario != null) {
-	        // Handle case where a record with the same codigo already exists
-	    	 throw new ItinerarioExistenteException(codigo);
-	    }
-		
+		ItinerariosFormativos existingItinerario = mantenimientoItinerariosFormativosRepository.findByCodigo(codigo);
+
+		if (existingItinerario != null) {
+			// Handle case where a record with the same codigo already exists
+			throw new ItinerarioExistenteException(codigo);
+		}
+
 	}
-
-
-
 
 }
