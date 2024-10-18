e88bd5fda826b6c519cc20414c579fbae539c9ab
prosgome <pedro-jose.ros-gomez@capgemini.com>
2024-10-18 09:32:46 +0200

    Revert "implementacion para  carga de archivos Certificados en curos"
    This reverts commit 78f075a72a0f712f63a3137593b372e4b9ceab5f.

--- src/main/java/com/ccsw/capabilitymanager/certificatesversion/CertificatesVersionRepository.java
+++ src/main/java/com/ccsw/capabilitymanager/certificatesversion/CertificatesVersionRepository.java
@@ -2,14 +2,10 @@ package com.ccsw.capabilitymanager.certificatesversion;
 
 import java.util.Optional;
 
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ccsw.capabilitymanager.certificatesversion.model.CertificatesVersion;
-import org.springframework.stereotype.Service;
 
-@Service
-@Transactional
 public interface CertificatesVersionRepository extends JpaRepository<CertificatesVersion, Long> {
 
 	Optional<CertificatesVersion> findById(Long id);
